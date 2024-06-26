e763268ef7d8d845d4200a8e2e595a176808eae6
iamsourav37 <sourav.g29@outlook.com>
2024-06-26 21:02:50 +0530

    first endpoint

---
+++ MagicVilla.API/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla.API.Models;
+using MagicVilla.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace MagicVilla.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO>()
+            {
+                new VillaDTO(){Id=1, Name="Panch Pakhori"},
+                new VillaDTO(){Id=2, Name="Maa Homestay"},
+                new VillaDTO(){Id=3, Name="Megh Bitan"},
+            };
+        }
+    }
+}
